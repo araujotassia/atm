3a5a9ac6227d3dcd89a18b9cf4312f0561bc91df
tassia <araujot@vanier.college>
2020-04-27 22:36:03 -0400

    Adding Account class

---
+++ src/atm/Account.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atm;
+
+/**
+ *
+ * @author Tassia Camoes Araujo &lt;araujot@vanier.college&gt;
+ */
+public class Account {
+    
+}
